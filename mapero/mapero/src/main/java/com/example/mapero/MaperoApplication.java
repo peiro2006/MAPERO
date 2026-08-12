b1091d03345b9184e5e0c6de1f13ec8022461347
peiro2006 <peironenazarenoa@gmail.com>
2026-08-12 14:27:24 -0300

    Bases Front y Backend

---
+++ mapero/mapero/src/main/java/com/example/mapero/MaperoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaperoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaperoApplication.class, args);
+	}
+
+}
---
+++ mapero/mapero/src/test/java/com/example/mapero/MaperoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaperoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
